f53b028f5afe0d8caa08d05f26db978189b82e53
Benjamin Hidalgo <benjaminhidalgo123@gmail.com>
2025-06-01 23:12:30 -0400

    Base API Vendedor

---
+++ src/main/java/com/api/apivendedor/ApivendedorApplication.java
@@ -0,0 +1,13 @@
+package com.api.apivendedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApivendedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApivendedorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/apivendedor/ApivendedorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.apivendedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApivendedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
